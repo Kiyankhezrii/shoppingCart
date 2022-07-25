425eedb2b6a94c9dc1786353618ab398709f91bf
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-25 17:22:17 +0430

    addCart to modal function was added

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { productsData } from "./products.js";
 
 // get element from DOM
 const shopping = document.querySelector(".shooping");
+const numShopping = shopping.querySelector("span");
 const modalContainer = document.querySelectorAll(".modal-container");
 const modal = document.querySelector(".modal");
 const modalBtn = document.querySelector(".btns");
@@ -42,19 +43,43 @@ class Ui {
       const storage = Storage.getCartStorage();
       const filterCart =
         storage?.find((c) => c.id == id) || cart.find((c) => c.id == id);
+
       if (!filterCart) {
         btn.addEventListener("click", () => {
           btn.textContent = "In cart";
           btn.disable = true;
           const newP = productsData.find((p) => p.id == id);
-          cart = [...cart, { ...newP, quantity: 1 }];
-          Storage.setCartStorage([...cart]);
+          const findP = [{ ...newP, quantity: 1 }];
+          const what = storage ? storage : cart;
+          cart = [...what, ...findP];
+          this.addCartModal(findP);
+          numShopping.textContent = Number(numShopping.textContent) + 1;
+          Storage.setCartStorage(cart);
         });
       } else {
         btn.textContent = "In cart";
         btn.disable = true;
+        numShopping.textContent = Number(numShopping.textContent) + 1;
+        this.addCartModal([filterCart]);
       }
     });
+  }
+  addCartModal(c) {
+    const html = `
+        <div class="modal-container">
+            <div class="modal-img">
+            <img src="./images/product-${c[0].id}.jpeg" alt="p-${c[0].id}" class="modal__img" />
+            </div>
+            <p class="modal__price">${c[0].price} $</p>
+            <div class="cehvron">
+            <i class="fa-solid fa-chevron-up" data-id=${c[0].id}></i>
+            <p>${c[0].quantity}</p>
+            <i class="fa-solid fa-chevron-down" data-id=${c[0].id}></i>
+            </div>
+            <i class="fa-solid fa-trash-can"></i>
+      </div>
+        `;
+    modal.insertAdjacentHTML("afterbegin", html);
   }
 }
 
@@ -85,18 +110,22 @@ document.addEventListener("DOMContentLoaded", () => {
     pro.forEach((p) => ui.renderProducts(p));
     Storage.setProductsStorage(pro);
   }
-  //   const cartItems = Storage.getCartStorage() || [];
   ui.getButtons();
 });
 
 // modal and overlay
-modalBtn.addEventListener("click", (e) => {
-  if (e.target.classList.contains("clear")) {
-    modalContainer.forEach((mc) => mc.remove());
-    modal.classList.add("hidden");
-    overlay.style.opacity = "1";
-    overlay.style.display = "none";
-  }
+const closeModal = function () {
+  modalContainer.forEach((mc) => mc.remove());
+  modal.classList.add("hidden");
+  overlay.style.opacity = "1";
+  overlay.style.display = "none";
+};
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("clear")) closeModal();
+});
+overlay.addEventListener("click", () => {
+  closeModal();
 });
 
 shopping.addEventListener("click", (e) => {
